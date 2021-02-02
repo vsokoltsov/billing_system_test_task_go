cc3110ae005a46c685abc554e2f09a485ccfb8b4
Vadim Sokoltsov <vforvad@gmail.com>
2021-02-02 16:18:55 +0300

    Add templates for packages

---
+++ pkg/users/handlers.go
@@ -0,0 +1,14 @@
+package user
+
+import "net/http"
+
+type UsersHandler struct {
+}
+
+func (uh *UsersHandler) Create(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (uh *UsersHandler) Enroll(w http.ResponseWriter, r *http.Request) {
+
+}
---
+++ pkg/wallets/handlers.go
@@ -0,0 +1,10 @@
+package wallets
+
+import "net/http"
+
+type WalletsHandler struct {
+}
+
+func (wh *WalletsHandler) Transfer(w http.ResponseWriter, r *http.Request) {
+
+}
