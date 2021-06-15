9ed279fd37cc9c3f24350ab74115ae90a899b873
Vadim Sokoltsov <vforvad@gmail.com>
2021-06-15 23:30:03 +0300

    Add utils package

---
+++ pkg/utils/utils.go
@@ -0,0 +1,34 @@
+package utils
+
+import (
+	"encoding/json"
+	"net/http"
+	"strings"
+
+	"github.com/go-playground/validator"
+)
+
+func JsonResponseError(w http.ResponseWriter, status int, message string) {
+	w.WriteHeader(status)
+	json.NewEncoder(w).Encode(map[string]string{
+		"message": message,
+	})
+}
+
+// ValidateForm performs base form validation
+func ValidateForm(form interface{}, formErrors map[string][]string) map[string][]string {
+	validate := validator.New()
+	errValidate := validate.Struct(form)
+	if errValidate != nil {
+		for _, err := range errValidate.(validator.ValidationErrors) {
+			errKey := strings.ToLower(err.Field())
+			_, ok := formErrors[errKey]
+			if ok {
+				formErrors[errKey] = append(formErrors[errKey], err.Tag())
+			} else {
+				formErrors[errKey] = []string{err.Tag()}
+			}
+		}
+	}
+	return formErrors
+}
