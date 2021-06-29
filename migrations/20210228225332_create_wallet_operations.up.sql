baedd22161b6ce01e66ee95f2d78cf7f2315b257
Vadim Sokoltsov <vforvad@gmail.com>
2021-06-29 22:16:08 +0300

    Fix migration for wallet_operations table

--- migrations/20210228225332_create_wallet_operations.up.sql
+++ migrations/20210228225332_create_wallet_operations.up.sql
@@ -1,8 +1,8 @@
 create table wallet_operations (
     id SERIAL PRIMARY KEY,
-    opertion varchar(100),
+    operation varchar(100),
     wallet_from int,
     wallet_to int,
-    amount numeric(10, 2) NOT NULL default 0.00 constraint positive_balance CHECK(amount > 0),
+    amount numeric(10, 2) NOT NULL default 0.00 constraint positive_balance CHECK(amount >= 0),
     created_at timestamp without time zone default current_timestamp
 );
