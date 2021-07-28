5a57d7dd1db7ab3c0f55570b92bb77bc633db3b7
Vadim Sokoltsov <vforvad@gmail.com>
2021-07-29 01:16:10 +0300

    Implement saving reports to file and returning it

--- pkg/operations/handlers.go
+++ pkg/operations/handlers.go
@@ -1,9 +1,16 @@
 package operations
 
 import (
+	"bufio"
+	"context"
+	"encoding/json"
+	"fmt"
 	"io"
 	"net/http"
 	"os"
+	"path/filepath"
+	"runtime"
+	"strconv"
 )
 
 type OperationsHandler struct {
@@ -20,9 +27,58 @@ type OperationsHandler struct {
 // @Header 200 {string} Content-Type "application/octet-stream"
 // @Header 200 {string} Expires "0"
 func (oh *OperationsHandler) List(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/octet-stream")
-	w.Header().Set("Content-Disposition", "attachment; filename="+"test.txt")
-	f, _ := os.OpenFile("./test.txt", os.O_WRONLY|os.O_CREATE, 0666)
+	ctx := context.Background()
+
+	_, b, _, _ := runtime.Caller(0)
+	basepath := filepath.Dir(b)
+	f, err := os.OpenFile(filepath.Join(basepath, "report.txt"), os.O_CREATE|os.O_RDWR|os.O_APPEND, 0666)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 	defer f.Close()
+
+	rows, rowsErr := oh.OperationsRepo.List(ctx)
+	if rowsErr != nil {
+		fmt.Println(rowsErr)
+		return
+	}
+
+	for rows.Next() {
+		operation := WalletOperation{}
+		scanErr := rows.Scan(&operation.ID, &operation.Operation, &operation.WalletFrom, &operation.WalletTo, &operation.Amount, &operation.CreatedAt)
+		if scanErr != nil {
+			fmt.Println("SCAN ERROR: ", scanErr)
+			return
+		}
+		jsonBytes, jsonMarshallErr := json.Marshal(operation)
+		if jsonMarshallErr != nil {
+			fmt.Println("SCAN ERROR: ", jsonMarshallErr)
+			return
+		}
+		newLine := []byte("\n")
+		_, writeErr := f.Write(jsonBytes)
+		f.Write(newLine)
+		if writeErr != nil {
+			fmt.Println("Write err: ", writeErr)
+			return
+		}
+	}
+
+	header := make([]byte, 512)
+	f.Read(header)
+	stat, _ := f.Stat()
+	size := strconv.FormatInt(stat.Size(), 10)
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		line := scanner.Text()
+		fmt.Println(line)
+	}
+	f.Seek(0, 0)
+	w.Header().Set("Content-Disposition", "attachment; filename="+"report.txt")
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Content-Length", size)
+
 	io.Copy(w, f)
 }
