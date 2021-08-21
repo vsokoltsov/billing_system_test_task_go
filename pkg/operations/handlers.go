89a8943867e1ae89ea8626391099b36c2af98b50
Vadim Sokoltsov <vforvad@gmail.com>
2021-08-21 11:46:42 +0300

    Move pipes into separated file

--- pkg/operations/handlers.go
+++ pkg/operations/handlers.go
@@ -1,11 +1,11 @@
 package operations
 
 import (
+	"billing_system_test_task/pkg/pipeline"
 	"bufio"
 	"context"
 	"encoding/csv"
 	"fmt"
-	"log"
 	"net/http"
 	"os"
 	"path/filepath"
@@ -13,39 +13,6 @@ import (
 	"strconv"
 	"sync"
 )
-
-type ReadPipe struct {
-	OperationsRepo IWalletOperationRepo
-	jobs           <-chan ReadJob
-}
-
-func (rp *ReadPipe) Call(in, out interface{}) {
-
-}
-
-type MarshallPipe struct {
-}
-
-func (mp *MarshallPipe) Call(in, out interface{}) {
-
-}
-
-type WritePipe struct {
-}
-
-func (wp *WritePipe) Call(in, out interface{}) {
-
-}
-
-type ReadJob struct {
-	format string
-	wo     *WalletOperation
-}
-
-type MarshalledResult struct {
-	id   int
-	data interface{}
-}
 
 type OperationsHandler struct {
 	OperationsRepo IWalletOperationRepo
@@ -70,11 +37,8 @@ func (oh *OperationsHandler) List(w http.ResponseWriter, r *http.Request) {
 		fileOpenErr    error
 		listParams     *ListParams
 		operationsChan = make(chan *WalletOperation, 1)
-		marshalledChan = make(chan MarshalledResult, 1)
 		waitGroup      = &sync.WaitGroup{}
-		errors         = make(chan error)
 		fileMutex      = &sync.Mutex{}
-		resultChan     = make(chan bool)
 		fileHandler    IFileMarshaller
 	)
 	v := r.URL.Query()
@@ -137,101 +101,31 @@ func (oh *OperationsHandler) List(w http.ResponseWriter, r *http.Request) {
 			mu:   fileMutex,
 		}
 	}
-	waitGroup.Add(1)
-	go func(handler *OperationsHandler, oc chan *WalletOperation, wg *sync.WaitGroup, errors chan error) {
-		rows, rowsErr := oh.OperationsRepo.List(ctx, listParams)
+	readPipe := ReadPipe{
+		or:     oh.OperationsRepo,
+		oc:     operationsChan,
+		wg:     waitGroup,
+		params: listParams,
+		ctx:    ctx,
+	}
+	MarshallPipe := MarshallPipe{
+		wg: waitGroup,
+		fm: fileHandler,
+	}
+	WritePipe := WritePipe{
+		wg: waitGroup,
+		fm: fileHandler,
+	}
+	pipes := []pipeline.Pipe{
+		readPipe,
+		MarshallPipe,
+		WritePipe,
+	}
 
-		defer close(oc)
-		defer wg.Done()
-
-		if rowsErr != nil {
-			fmt.Println(rowsErr)
-			return
-		}
-		for rows.Next() {
-			operation := WalletOperation{}
-			scanErr := rows.Scan(&operation.ID, &operation.Operation, &operation.WalletFrom, &operation.WalletTo, &operation.Amount, &operation.CreatedAt)
-			if scanErr != nil {
-				log.Fatalf("Error of row scanning: %s", scanErr)
-			}
-			oc <- &operation
-		}
-
-	}(oh, operationsChan, waitGroup, errors)
-
-	waitGroup.Add(1)
-	go func(oc chan *WalletOperation, mc chan MarshalledResult, wg *sync.WaitGroup, format string, fileHandler IFileMarshaller, errors chan error) {
-		defer close(mc)
-		defer wg.Done()
-
-		for operation := range operationsChan {
-			mr, mrErr := fileHandler.MarshallOperation(operation)
-			if mrErr != nil {
-				log.Printf("[ERROR] Marshalling error: %s", mrErr)
-			} else {
-				mc <- *mr
-			}
-		}
-	}(operationsChan, marshalledChan, waitGroup, format, fileHandler, errors)
-
-	waitGroup.Add(1)
-	go func(mc chan MarshalledResult, mu *sync.Mutex, wg *sync.WaitGroup, jsonFile *os.File, csvWriter *csv.Writer, format string, fileHandler IFileMarshaller, errors chan error) {
-		defer func(iGroup *sync.WaitGroup, res chan bool) {
-			iGroup.Done()
-		}(wg, resultChan)
-
-		for mr := range mc {
-			fileHandler.WriteToFile(&mr)
-		}
-	}(marshalledChan, fileMutex, waitGroup, f, csvWriter, format, fileHandler, errors)
+	waitGroup.Add(3)
+	pipeline.ExecutePipeline(pipes...)
 
 	waitGroup.Wait()
-	// }(waitGroup)
-
-	// //
-	// <-resultChan
-	// for md := range marshalledChan {
-	// 	fmt.Println(md)
-	// 	// operations = append(operations, operation)
-	// 	// fmt.Println(operation)
-	// }
-	// fmt.Println(operations)
-	// for rows.Next() {
-	// 	operation := WalletOperation{}
-	// 	scanErr := rows.Scan(&operation.ID, &operation.Operation, &operation.WalletFrom, &operation.WalletTo, &operation.Amount, &operation.CreatedAt)
-	// 	if scanErr != nil {
-	// 		fmt.Println("SCAN ERROR: ", scanErr)
-	// 		return
-	// 	}
-	// 	if format == "json" {
-	// jsonBytes, jsonMarshallErr := json.Marshal(operation)
-	// if jsonMarshallErr != nil {
-	// 	fmt.Println("SCAN ERROR: ", jsonMarshallErr)
-	// 	return
-	// }
-	// newLine := []byte("\n")
-	// _, writeErr := f.Write(jsonBytes)
-	// f.Write(newLine)
-	// if writeErr != nil {
-	// 	fmt.Println("Write err: ", writeErr)
-	// 	return
-	// }
-	// 	} else if format == "csv" && csvWriter != nil {
-	// idStr := strconv.Itoa(operation.ID)
-	// walletFromStr := strconv.Itoa(int(operation.WalletFrom.Int32))
-	// walletToStr := strconv.Itoa(int(operation.WalletTo))
-	// amountStr := operation.Amount.String()
-	// createdAtStr := operation.CreatedAt.String()
-	// row := []string{
-	// 	idStr,
-	// 	walletFromStr,
-	// 	walletToStr,
-	// 	amountStr,
-	// 	createdAtStr,
-	// }
-	// 		csvWriter.Write(row)
-	// 	}
-	// }
 
 	header := make([]byte, 512)
 	f.Read(header)
