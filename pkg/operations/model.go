328453fe96b5eb39afe337b010473553b0efb558
Vadim Sokoltsov <vforvad@gmail.com>
2021-08-27 13:37:09 +0300

    Add model json handling

--- pkg/operations/model.go
+++ pkg/operations/model.go
@@ -1,16 +1,18 @@
 package operations
 
 import (
+	"database/sql"
 	"time"
 
 	"github.com/shopspring/decimal"
 )
 
+//easyjson:json
 type WalletOperation struct {
-	ID         int
-	Operation  string
-	WalletFrom int
-	WalletTo   int
-	Amount     decimal.Decimal
-	CreatedAt  time.Time
+	ID         int             `json:"id"`
+	Operation  string          `json:"operation"`
+	WalletFrom sql.NullInt32   `json:"wallet_from"`
+	WalletTo   int             `json:"wallet_to"`
+	Amount     decimal.Decimal `json:"amount"`
+	CreatedAt  time.Time       `json:"created_at"`
 }
