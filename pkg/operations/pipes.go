f7ed6fc46899bfbad220f8568e52840229af7d00
Vadim Sokoltsov <vforvad@gmail.com>
2021-08-28 14:45:10 +0300

    Add tests for pipes module

--- pkg/operations/pipes.go
+++ pkg/operations/pipes.go
@@ -2,13 +2,13 @@ package operations
 
 import (
 	"context"
-	"log"
+	"fmt"
 	"sync"
 )
 
 type ReadPipe struct {
 	or     IWalletOperationRepo
-	oc     chan *WalletOperation
+	errors chan error
 	wg     *sync.WaitGroup
 	params *ListParams
 	ctx    context.Context
@@ -16,52 +16,76 @@ type ReadPipe struct {
 
 func (rp ReadPipe) Call(in, out chan interface{}) {
 	defer rp.wg.Done()
+	var counter int
 
 	rows, rowsErr := rp.or.List(rp.ctx, rp.params)
 	if rowsErr != nil {
-		log.Printf("Error of row retrieving: %s", rowsErr)
+		rp.errors <- fmt.Errorf("error of row retrieving: %s", rowsErr)
+		out <- nil
 		return
 	}
+	defer rows.Close()
+
 	for rows.Next() {
 		operation := WalletOperation{}
 		scanErr := rows.Scan(&operation.ID, &operation.Operation, &operation.WalletFrom, &operation.WalletTo, &operation.Amount, &operation.CreatedAt)
 		if scanErr != nil {
-			log.Fatalf("Error of row scanning: %s", scanErr)
+			rp.errors <- fmt.Errorf("error of row scanning: %s", scanErr)
+			out <- nil
+			return
 		}
+		counter++
 		out <- &operation
+	}
+	if counter == 0 {
+		out <- nil
 	}
 }
 
 type MarshallPipe struct {
-	wg *sync.WaitGroup
-	fm IFileMarshaller
+	wg     *sync.WaitGroup
+	fm     IFileMarshaller
+	errors chan error
 }
 
 func (mp MarshallPipe) Call(in, out chan interface{}) {
 	defer mp.wg.Done()
 
 	for chunk := range in {
+		if chunk == nil {
+			out <- nil
+			return
+		}
 		operation := chunk.(*WalletOperation)
 		mr, mrErr := mp.fm.MarshallOperation(operation)
 		if mrErr != nil {
-			log.Printf("[ERROR] Marshalling error: %s", mrErr)
-		} else {
-			out <- *mr
+			out <- nil
+			err := fmt.Errorf("[ERROR] Marshalling error: %s", mrErr)
+			mp.errors <- err
+			return
 		}
+		out <- mr
 	}
 }
 
 type WritePipe struct {
-	wg *sync.WaitGroup
-	fm IFileMarshaller
+	wg     *sync.WaitGroup
+	fm     IFileMarshaller
+	errors chan error
 }
 
 func (wp WritePipe) Call(in, out chan interface{}) {
 	defer wp.wg.Done()
 
 	for chunk := range in {
-		mr := chunk.(MarshalledResult)
-		wp.fm.WriteToFile(&mr)
+		if chunk != nil {
+			mr := chunk.(*MarshalledResult)
+			writeErr := wp.fm.WriteToFile(mr)
+			if writeErr != nil {
+				wp.errors <- fmt.Errorf("[ERROR] Write to file error: %s", writeErr)
+				return
+			}
+		}
 	}
 }
 
---
+++ pkg/operations/pipes_test.go
@@ -0,0 +1,384 @@
+package operations
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/golang/mock/gomock"
+	"github.com/shopspring/decimal"
+)
+
+// Test success read pipe rows receiving
+func TestSuccessReadPipe(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer db.Close()
+
+	errCh := make(chan error)
+	or := NewWalletOperationRepo(db)
+	wg := &sync.WaitGroup{}
+	ctx := context.Background()
+	in := make(chan interface{}, 1)
+	out := make(chan interface{}, 1)
+	op := WalletOperation{
+		ID:         1,
+		Operation:  "deposit",
+		WalletFrom: sql.NullInt32{Int32: 1},
+		WalletTo:   2,
+		Amount:     decimal.NewFromInt(100),
+		CreatedAt:  time.Now(),
+	}
+
+	rows := sqlmock.NewRows([]string{"id", "operation", "wallet_from", "wallet_to", "amount", "created_at"})
+	rows = rows.AddRow(op.ID, op.Operation, op.WalletFrom, op.WalletTo, op.Amount, op.CreatedAt)
+	mock.ExpectQuery("select").WillReturnRows(rows)
+
+	readPipe := ReadPipe{
+		or:     or,
+		wg:     wg,
+		params: nil,
+		ctx:    ctx,
+		errors: errCh,
+	}
+
+	wg.Add(1)
+	go readPipe.Call(in, out)
+	wg.Wait()
+
+	res := <-out
+	chanOp := res.(*WalletOperation)
+	if chanOp.ID != op.ID {
+		t.Errorf("Received ID do not match. Expected %d, got %d", op.ID, chanOp.ID)
+	}
+}
+
+// Test failed read pipe rows receiving (rows error)
+func TestFailedReadPipeRowsError(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer db.Close()
+
+	errCh := make(chan error, 1)
+	or := NewWalletOperationRepo(db)
+	wg := &sync.WaitGroup{}
+	ctx := context.Background()
+	in := make(chan interface{}, 1)
+	out := make(chan interface{}, 1)
+
+	mock.ExpectQuery("select").WillReturnError(fmt.Errorf("log error"))
+
+	readPipe := ReadPipe{
+		or:     or,
+		wg:     wg,
+		params: nil,
+		ctx:    ctx,
+		errors: errCh,
+	}
+
+	wg.Add(1)
+	go readPipe.Call(in, out)
+	wg.Wait()
+	// close(out)
+
+	err = <-errCh
+	if err == nil {
+		t.Errorf("Expected error, got nil")
+	}
+
+	nilData := <-out
+	if nilData != nil {
+		t.Errorf("Expected nil value, got %s", nilData)
+	}
+}
+
+// Test failed read pipe rows receiving (scan error)
+func TestFailedReadPipeScanError(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer db.Close()
+
+	errCh := make(chan error, 1)
+	or := NewWalletOperationRepo(db)
+	wg := &sync.WaitGroup{}
+	ctx := context.Background()
+	in := make(chan interface{}, 1)
+	out := make(chan interface{}, 1)
+
+	op := WalletOperation{
+		ID:         1,
+		Operation:  "deposit",
+		WalletFrom: sql.NullInt32{Int32: 1},
+		WalletTo:   2,
+		Amount:     decimal.NewFromInt(100),
+		CreatedAt:  time.Now(),
+	}
+	rows := sqlmock.NewRows([]string{"id", "operation", "wallet_from", "wallet_to", "amount", "created_at"})
+	rows = rows.AddRow(nil, op.Operation, op.WalletFrom, op.WalletTo, op.Amount, op.CreatedAt).RowError(1, fmt.Errorf("Scan error"))
+	mock.ExpectQuery("select").WillReturnRows(rows)
+
+	readPipe := ReadPipe{
+		or:     or,
+		wg:     wg,
+		params: nil,
+		ctx:    ctx,
+		errors: errCh,
+	}
+
+	wg.Add(1)
+	go readPipe.Call(in, out)
+	wg.Wait()
+
+	err = <-errCh
+	if err == nil {
+		t.Errorf("Expected error, got nil")
+	}
+
+	nilData := <-out
+	if nilData != nil {
+		t.Errorf("Expected nil value, got %s", nilData)
+	}
+}
+
+// Test failed read pipe rows receiving (query returns emtpy result)
+func TestFailedReadPipeEmptyQuery(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer db.Close()
+
+	errCh := make(chan error, 1)
+	or := NewWalletOperationRepo(db)
+	wg := &sync.WaitGroup{}
+	ctx := context.Background()
+	in := make(chan interface{}, 1)
+	out := make(chan interface{}, 1)
+
+	rows := sqlmock.NewRows([]string{"id", "operation", "wallet_from", "wallet_to", "amount", "created_at"})
+	mock.ExpectQuery("select").WillReturnRows(rows)
+
+	readPipe := ReadPipe{
+		or:     or,
+		wg:     wg,
+		params: nil,
+		ctx:    ctx,
+		errors: errCh,
+	}
+
+	wg.Add(1)
+	go readPipe.Call(in, out)
+	// close(in)
+	wg.Wait()
+
+	nilRes := <-out
+	if nilRes != nil {
+		t.Errorf("Expected nil result, got %s", nilRes)
+	}
+}
+
+// Test success marshall pipe json marshalling
+func TestSuccessMarshallPipe(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	mockFileMarshaller := NewMockIFileMarshaller(ctrl)
+	errCh := make(chan error, 1)
+	wg := &sync.WaitGroup{}
+	in := make(chan interface{}, 1)
+	out := make(chan interface{}, 1)
+	op := WalletOperation{
+		ID:         1,
+		Operation:  "deposit",
+		WalletFrom: sql.NullInt32{Int32: 1},
+		WalletTo:   2,
+		Amount:     decimal.NewFromInt(100),
+		CreatedAt:  time.Now(),
+	}
+
+	marshallPipe := MarshallPipe{
+		wg:     wg,
+		errors: errCh,
+		fm:     mockFileMarshaller,
+	}
+
+	mockFileMarshaller.EXPECT().MarshallOperation(&op).Return(&MarshalledResult{
+		id:   op.ID,
+		data: op,
+	}, nil)
+
+	in <- &op
+	wg.Add(1)
+	go marshallPipe.Call(in, out)
+	close(in)
+	wg.Wait()
+
+	res := <-out
+	chanOp := res.(*MarshalledResult)
+	if chanOp.id != op.ID {
+		t.Errorf("Received ID do not match. Expected %d, got %d", op.ID, chanOp.id)
+	}
+}
+
+// Test failed marshall pipe json marshalling (error marshalling)
+func TestFailedMarshallPipeErrorMarshalling(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	mockFileMarshaller := NewMockIFileMarshaller(ctrl)
+	errCh := make(chan error, 1)
+	wg := &sync.WaitGroup{}
+	in := make(chan interface{}, 1)
+	out := make(chan interface{}, 1)
+	op := WalletOperation{
+		ID:         1,
+		Operation:  "deposit",
+		WalletFrom: sql.NullInt32{Int32: 1},
+		WalletTo:   2,
+		Amount:     decimal.NewFromInt(100),
+		CreatedAt:  time.Now(),
+	}
+
+	marshallPipe := MarshallPipe{
+		wg:     wg,
+		errors: errCh,
+		fm:     mockFileMarshaller,
+	}
+
+	mockFileMarshaller.EXPECT().MarshallOperation(&op).Return(nil, fmt.Errorf("marshall error"))
+
+	in <- &op
+	wg.Add(1)
+	go marshallPipe.Call(in, out)
+	wg.Wait()
+
+	err := <-errCh
+	res := <-out
+	if err == nil {
+		t.Errorf("Expected error, got nil")
+	}
+
+	if res != nil {
+		t.Errorf("Expected nil result, got %s", res)
+	}
+}
+
+// Test failed marshall pipe json marshalling (previous step cancelled)
+func TestFailedMarshallPipePrevPipeCancelled(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	mockFileMarshaller := NewMockIFileMarshaller(ctrl)
+	errCh := make(chan error)
+	wg := &sync.WaitGroup{}
+	in := make(chan interface{}, 1)
+	out := make(chan interface{}, 1)
+
+	marshallPipe := MarshallPipe{
+		wg:     wg,
+		errors: errCh,
+		fm:     mockFileMarshaller,
+	}
+
+	in <- nil
+
+	wg.Add(1)
+	go marshallPipe.Call(in, out)
+	wg.Wait()
+
+	res := <-out
+
+	if res != nil {
+		t.Errorf("Expected nil result, got %s", res)
+	}
+}
+
+// Test success write pipe file writing
+func TestSuccessWritePipe(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	mockFileMarshaller := NewMockIFileMarshaller(ctrl)
+	errCh := make(chan error)
+	wg := &sync.WaitGroup{}
+	in := make(chan interface{}, 1)
+	out := make(chan interface{}, 1)
+	op := WalletOperation{
+		ID:         1,
+		Operation:  "deposit",
+		WalletFrom: sql.NullInt32{Int32: 1},
+		WalletTo:   2,
+		Amount:     decimal.NewFromInt(100),
+		CreatedAt:  time.Now(),
+	}
+	mr := MarshalledResult{
+		id:   op.ID,
+		data: op,
+	}
+
+	writePipe := WritePipe{
+		wg:     wg,
+		errors: errCh,
+		fm:     mockFileMarshaller,
+	}
+
+	in <- &mr
+	mockFileMarshaller.EXPECT().WriteToFile(&mr).Return(nil)
+
+	wg.Add(1)
+	go writePipe.Call(in, out)
+	close(in)
+	wg.Wait()
+
+	select {
+	case err := <-errCh:
+		t.Errorf("Unexpected error %s", err)
+	case nilData := <-out:
+		t.Errorf("Unexpected nil data from out %s", nilData)
+	default:
+		fmt.Println("Success file write")
+	}
+}
+
+// Test failed write pipe file writing (write file error)
+func TestFailedWritePipe(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	mockFileMarshaller := NewMockIFileMarshaller(ctrl)
+	errCh := make(chan error, 1)
+	wg := &sync.WaitGroup{}
+	in := make(chan interface{}, 1)
+	out := make(chan interface{}, 1)
+	op := WalletOperation{
+		ID:         1,
+		Operation:  "deposit",
+		WalletFrom: sql.NullInt32{Int32: 1},
+		WalletTo:   2,
+		Amount:     decimal.NewFromInt(100),
+		CreatedAt:  time.Now(),
+	}
+	mr := MarshalledResult{
+		id:   op.ID,
+		data: op,
+	}
+
+	writePipe := WritePipe{
+		wg:     wg,
+		errors: errCh,
+		fm:     mockFileMarshaller,
+	}
+
+	in <- &mr
+	mockFileMarshaller.EXPECT().WriteToFile(&mr).Return(fmt.Errorf("File error"))
+
+	wg.Add(1)
+	go writePipe.Call(in, out)
+	close(in)
+	wg.Wait()
+
+	err := <-errCh
+	if err == nil {
+		t.Errorf("Expected error, got nil")
+	}
+}
