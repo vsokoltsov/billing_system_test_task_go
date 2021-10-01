5b40ec1fc625fd5c2ba502b2eb3749433eb86578
Vadim Sokoltsov <vforvad@gmail.com>
2021-10-02 00:36:50 +0300

    Add sql adapter interface

---
+++ pkg/adapters/sql.go
@@ -0,0 +1,12 @@
+package adapters
+
+import (
+	"context"
+	"database/sql"
+)
+
+type SQLAdapter interface {
+	QueryContext(ctx context.Context, query string, args ...interface{}) (*sql.Rows, error)
+	ExecContext(ctx context.Context, query string, args ...interface{}) (sql.Result, error)
+	BeginTx(ctx context.Context, opts *sql.TxOptions) (*sql.Tx, error)
+}
