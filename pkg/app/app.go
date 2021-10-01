47757a605cffcd84160527dfb51f010b546db900
Vadim Sokoltsov <vforvad@gmail.com>
2021-10-02 01:18:07 +0300

    Changed application structure

--- pkg/app/app.go
+++ pkg/app/app.go
@@ -1,8 +1,11 @@
 package app
 
 import (
-	"billing_system_test_task/pkg/api"
+	"billing_system_test_task/pkg/adapters/tx"
 	"billing_system_test_task/pkg/entities"
+	"billing_system_test_task/pkg/repositories"
+	httpHandlers "billing_system_test_task/pkg/transport/http"
+	"billing_system_test_task/pkg/usecases"
 	"context"
 	"database/sql"
 	"log"
@@ -13,64 +16,60 @@ import (
 	"time"
 
 	"github.com/gorilla/handlers"
-	"github.com/gorilla/mux"
 	_ "github.com/lib/pq"
 )
 
 type AppAdapter interface {
-	Initialize()
 	Run()
 }
 
 // App represents base application info
 type App struct {
-	config        entities.ConfigAdapter
-	host          string
-	port          string
-	pathDelimiter string
-	router        *mux.Router
-	server        *http.Server
-	wait          time.Duration
+	config entities.ConfigAdapter
+	host   string
+	port   string
+	server *http.Server
+	wait   time.Duration
 }
 
 func NewApp(config entities.ConfigAdapter) AppAdapter {
-	return App{
-		config: config,
-	}
-}
-
-// App populates struct parameters with data
-func (a App) Initialize() {
 	var (
 		sqlDB        *sql.DB
 		sqlDbOpenErr error
+		host         = config.GetAppHost()
+		port         = config.GetAppPort()
+		dbProvider   = config.GetDBProvider()
+		dbConnString = config.GetDBConnectionString()
 	)
-
-	a.host = a.config.GetAppHost()
-	a.port = a.config.GetAppPort()
-	a.pathDelimiter = a.config.GetPathDelimiter()
-	a.wait = time.Second * 5
-
-	dbProvider := a.config.GetDBProvider()
-	dbConnString := a.config.GetDBConnectionString()
 
 	sqlDB, sqlDbOpenErr = sql.Open(dbProvider, dbConnString)
 	if sqlDbOpenErr != nil {
 		log.Fatalf("Error sql database open: %s", sqlDbOpenErr)
-		return
 	}
 	if pingErr := sqlDB.Ping(); pingErr != nil {
 		log.Fatalf("Error sql database connection: %s", pingErr)
 	}
 
-	a.router = api.SetUpRoutes(a.pathDelimiter, sqlDB)
+	txManger := tx.NewTxBeginner(sqlDB)
+	// walletsRepo := repositories.NewWalletService(sqlDB)
+	usersRepo := repositories.NewUsersService(sqlDB)
+	userInteractor := usecases.NewUserInteractor(usersRepo, txManger)
 
-	url := strings.Join([]string{a.host, a.port}, ":")
-	a.server = &http.Server{
-		Handler:      handlers.LoggingHandler(os.Stdout, a.router),
-		Addr:         url,
-		WriteTimeout: 15 * time.Second,
-		ReadTimeout:  15 * time.Second,
+	usersHandler := httpHandlers.NewUserHandler(userInteractor)
+	router := httpHandlers.NewRouter(*usersHandler)
+
+	url := strings.Join([]string{host, port}, ":")
+
+	return App{
+		host: host,
+		port: port,
+		wait: time.Second * 5,
+		server: &http.Server{
+			Handler:      handlers.LoggingHandler(os.Stdout, router),
+			Addr:         url,
+			WriteTimeout: 15 * time.Second,
+			ReadTimeout:  15 * time.Second,
+		},
 	}
 }
 
