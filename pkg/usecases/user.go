8be752eee1db2e11881fcb348c1c6fedde8b5609
Vadim Sokoltsov <vforvad@gmail.com>
2021-10-16 10:20:05 +0300

    Add errors adapter to user's mock

--- pkg/usecases/user.go
+++ pkg/usecases/user.go
@@ -1,6 +1,7 @@
 package usecases
 
 import (
+	"billing_system_test_task/pkg/adapters"
 	"billing_system_test_task/pkg/adapters/tx"
 	"billing_system_test_task/pkg/entities"
 	"billing_system_test_task/pkg/repositories"
@@ -11,71 +12,73 @@ import (
 
 // UserUseCase represents contracts for user's use cases
 type UserUseCase interface {
-	Create(ctx context.Context, email string) (*entities.User, error)
-	Enroll(ctx context.Context, userID int, amount decimal.Decimal) (*entities.User, error)
+	Create(ctx context.Context, email string) (*entities.User, adapters.Error)
+	Enroll(ctx context.Context, userID int, amount decimal.Decimal) (*entities.User, adapters.Error)
 }
 
 type UserInteractor struct {
+	errorsFactory     adapters.ErrorsFactory
 	userRepo          repositories.UsersManager
 	walletsRepo       repositories.WalletsManager
 	operationsManager repositories.OperationsManager
 	txManager         tx.TxBeginner
 }
 
-func NewUserInteractor(userRepo repositories.UsersManager, walletsRepo repositories.WalletsManager, operationsManager repositories.OperationsManager, txManager tx.TxBeginner) UserUseCase {
+func NewUserInteractor(userRepo repositories.UsersManager, walletsRepo repositories.WalletsManager, operationsManager repositories.OperationsManager, txManager tx.TxBeginner, errorsFactory adapters.ErrorsFactory) UserUseCase {
 	return UserInteractor{
 		userRepo:          userRepo,
 		walletsRepo:       walletsRepo,
 		txManager:         txManager,
 		operationsManager: operationsManager,
+		errorsFactory:     errorsFactory,
 	}
 }
 
 // Create creates new user, its wallet and operation for that event
-func (ui UserInteractor) Create(ctx context.Context, email string) (*entities.User, error) {
+func (ui UserInteractor) Create(ctx context.Context, email string) (*entities.User, adapters.Error) {
 
 	tx, txErr := ui.txManager.BeginTrx(ctx, nil)
 	if txErr != nil {
-		return nil, txErr
+		return nil, ui.errorsFactory.DefaultError(txErr)
 	}
 
 	txUserRepo := ui.userRepo.WithTx(tx)
 	userID, userErr := txUserRepo.Create(ctx, email)
 	if userErr != nil {
 		_ = tx.Rollback()
-		return nil, userErr
+		return nil, ui.errorsFactory.DefaultError(userErr)
 	}
 
 	walletID, walletErr := ui.walletsRepo.WithTx(tx).Create(ctx, userID)
 	if walletErr != nil {
 		_ = tx.Rollback()
-		return nil, walletErr
+		return nil, ui.errorsFactory.DefaultError(walletErr)
 	}
 
 	_, walletOperationErr := ui.operationsManager.WithTx(tx).Create(ctx, repositories.Create, 0, int(walletID), decimal.NewFromInt(0))
 	if walletOperationErr != nil {
 		_ = tx.Rollback()
-		return nil, walletOperationErr
+		return nil, ui.errorsFactory.DefaultError(walletOperationErr)
 	}
 
 	user, getUserErr := txUserRepo.GetByWalletID(ctx, int(walletID))
 	if getUserErr != nil {
 		_ = tx.Rollback()
-		return nil, getUserErr
+		return nil, ui.errorsFactory.NotFound(getUserErr)
 	}
 
 	if commitErr := tx.Commit(); commitErr != nil {
 		_ = tx.Rollback()
-		return nil, commitErr
+		return nil, ui.errorsFactory.DefaultError(commitErr)
 	}
 
 	return user, nil
 }
 
-func (ui UserInteractor) Enroll(ctx context.Context, userID int, amount decimal.Decimal) (*entities.User, error) {
+func (ui UserInteractor) Enroll(ctx context.Context, userID int, amount decimal.Decimal) (*entities.User, adapters.Error) {
 	tx, txErr := ui.txManager.BeginTrx(ctx, nil)
 	if txErr != nil {
-		return nil, txErr
+		return nil, ui.errorsFactory.DefaultError(txErr)
 	}
 
 	txUserRepo := ui.userRepo.WithTx(tx)
@@ -83,24 +86,24 @@ func (ui UserInteractor) Enroll(ctx context.Context, userID int, amount decimal.
 	user, getUserErr := txUserRepo.GetByID(ctx, userID)
 	if getUserErr != nil {
 		_ = tx.Rollback()
-		return nil, getUserErr
+		return nil, ui.errorsFactory.NotFound(getUserErr)
 	}
 
 	walletID, enrollWalletErr := ui.walletsRepo.WithTx(tx).Enroll(ctx, user.Wallet.ID, amount)
 	if enrollWalletErr != nil {
 		_ = tx.Rollback()
-		return nil, enrollWalletErr
+		return nil, ui.errorsFactory.DefaultError(enrollWalletErr)
 	}
 
 	enrolledUser, enrolledUserErr := txUserRepo.GetByWalletID(ctx, walletID)
 	if enrolledUserErr != nil {
 		_ = tx.Rollback()
-		return nil, enrolledUserErr
+		return nil, ui.errorsFactory.NotFound(enrolledUserErr)
 	}
 
 	if commitErr := tx.Commit(); commitErr != nil {
 		_ = tx.Rollback()
-		return nil, commitErr
+		return nil, ui.errorsFactory.DefaultError(commitErr)
 	}
 	return enrolledUser, nil
 }
