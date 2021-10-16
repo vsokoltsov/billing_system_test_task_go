38ebf234a42c32f3be51cb25202a08630c769eda
Vadim Sokoltsov <vforvad@gmail.com>
2021-10-16 10:19:31 +0300

    Add custom error adapter

---
+++ pkg/adapters/errors.go
@@ -0,0 +1,49 @@
+package adapters
+
+type Error interface {
+	GetError() error
+	GetStatus() int
+}
+
+type ErrorsFactory interface {
+	NotFound(err error) Error
+	DefaultError(err error) Error
+}
+
+type HTTPErrorsFactory struct{}
+
+func NewHTTPErrorsFactory() ErrorsFactory {
+	return &HTTPErrorsFactory{}
+}
+
+func (he *HTTPErrorsFactory) NotFound(err error) Error {
+	return NewHTTPError(
+		404, err,
+	)
+}
+
+func (he *HTTPErrorsFactory) DefaultError(err error) Error {
+	return NewHTTPError(
+		400, err,
+	)
+}
+
+type HTTPError struct {
+	status int
+	err    error
+}
+
+func NewHTTPError(status int, err error) Error {
+	return &HTTPError{
+		status: status,
+		err:    err,
+	}
+}
+
+func (err *HTTPError) GetError() error {
+	return err.err
+}
+
+func (err *HTTPError) GetStatus() int {
+	return err.status
+}
